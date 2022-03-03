e89c710dcb3f9a68a482ebbbff1b251268c26fe6
ViktorSedov60 <98118779+ViktorSedov60@users.noreply.github.com>
2022-03-03 07:08:57 +0300

    zad 13.2.1

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-print("hello")
-x = 5
-print(x)
+
+print(list(str(123456789)))
+# ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+print(list(map(int, ['1', '2', '3', '4', '5', '6', '7', '8', '9'])))
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+list_digit = list(map(int, list(str(123456789))))
+print(5 in list_digit)
+# True
+
+print('5' in str(123456789))
+# True
+
+print('3' and '7' in str(123456789))
+# True
+
+print('3' in str(123456789) and '7' in str(123456789))
+# True
