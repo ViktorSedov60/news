d62acf2dfa3dfbaa2d06efec70cfea932f115ce5
ViktorSedov60 <98118779+ViktorSedov60@users.noreply.github.com>
2022-03-08 21:40:43 +0300

    task13.8.19

--- test.py
+++ test.py
@@ -193,10 +193,10 @@ spees = 20
 # print(list(range(1, 10, 2)))
 # [1, 3, 5, 7, 9]
 
-S = 1  # заводим переменную-счетчик, в которой мы будем считать сумму
-N = 5
-
-# заводим цикл for, в котором мы будем проходить по всем числам от одного до N
+# S = 1  # заводим переменную-счетчик, в которой мы будем считать сумму
+# N = 5
+#
+# # заводим цикл for, в котором мы будем проходить по всем числам от одного до N
 # for i in range(1, N+1):  # равносильно выражению for i in [1, 2, 3, ... , N -1, N]:
 #     print("Значение суммы на предыдущем шаге: ", S)
 #     print("Текущее число: ", i)
@@ -216,13 +216,13 @@ N = 5
 #
 # print(P)
 
-# N = 7
+# N = 5
 #
 # for i in range(1, N + 1):
 #    print("*" * i)
 
 # S = 2  # заводим переменную-счетчик, в которой мы будем считать сумму
-n = 1  # текущее натуральное число
+# n = 1  # текущее натуральное число
 
 # заводим цикл while, который будет работать, пока сумма не превысит 500
 # while n ** 2 < 1000:  # делай пока ...
@@ -233,36 +233,161 @@ n = 1  # текущее натуральное число
 # # print("Сумма равна: ", S)
 # print("Количество чисел: ", n)
 
-random_matrix = [
-    [9, 2, 1],
-    [2, 5, 3],
-    [4, 8, 5]
-]
+# random_matrix = [
+#     [9, 2, 1],
+#     [2, 5, 3],
+#     [4, 8, 5]
+# ]
+#
+# mean_value_rows = []  # здесь будут храниться средние значения для каждой строки
+# min_value_rows = []  # здесь будут храниться минимальные значения для каждой строки
+# min_index_rows = []  # здесь будут храниться индексы минимальных значений для каждой строки
+# max_value_rows = []  # здесь будут храниться максимальные значения для каждой строки
+# max_index_rows = []  # здесь будут храниться индексы максимальных значений для каждой строки
+#
+# for row in random_matrix:  # здесь мы целиком берем каждую сроку
+#     min_index = 0  # в качестве минимального значения возьмем первый элемент строки
+#     max_index = 0
+#     min_value = row[min_index]  # начальное минимальное значение для каждой строки будет новое
+#     max_value = row[max_index]  # для максимального значения тоже самое
+#     for index_col in range(len(row)):
+#         if row[index_col] < min_value:
+#             min_value = row[index_col]
+#             min_index = index_col
+#         if row[index_col] > max_value:
+#             max_value = row[index_col]
+#             max_index = index_col
+#     min_value_rows.append(min_value)
+#     min_index_rows.append(min_index)
+#     max_value_rows.append(max_value)
+#     max_index_rows.append(max_index)
+#
+# print(min_value_rows)
+# print(min_index_rows)
+# print(max_value_rows)
+# print(max_index_rows)
 
-mean_value_rows = []  # здесь будут храниться средние значения для каждой строки
-min_value_rows = []  # здесь будут храниться минимальные значения для каждой строки
-min_index_rows = []  # здесь будут храниться индексы минимальных значений для каждой строки
-max_value_rows = []  # здесь будут храниться максимальные значения для каждой строки
-max_index_rows = []  # здесь будут храниться индексы максимальных значений для каждой строки
+# list_ = [-5, 2, 4, 8, 12, -7, 5]
+#
+# for i in range(len(list_)):  # равносильно выражению for i in [0, 1, 2, 3, 4, 5, 6]:
+#     print("Индекс элемента: ", i)
+#     print("Значение элемента: ", list_[i])  # с помощью индекса получаем значение элемента
+#     print("---")
+# print("Конец цикла")
 
-for row in random_matrix:  # здесь мы целиком берем каждую сроку
-    min_index = 0  # в качестве минимального значения возьмем первый элемент строки
-    max_index = 0
-    min_value = row[min_index]  # начальное минимальное значение для каждой строки будет новое
-    max_value = row[max_index]  # для максимального значения тоже самое
-    for index_col in range(len(row)):
-        if row[index_col] < min_value:
-            min_value = row[index_col]
-            min_index = index_col
-        if row[index_col] > max_value:
-            max_value = row[index_col]
-            max_index = index_col
-    min_value_rows.append(min_value)
-    min_index_rows.append(min_index)
-    max_value_rows.append(max_value)
-    max_index_rows.append(max_index)
+# list_ = [-5, 2, 4, 8, 12, -7, 5]
+# # Функция enumerate возвращает данные в виде кортежей,
+# # где на первом месте стоит индекс, а затем значение
+# # [(0, -5), (1, 2), (2, 4), ...]
+# for i, value in enumerate(list_):
+#     print("Индекс элемента: ", i)
+#     print("Значение элемента: ", value)  # с помощью индекса получаем значение элемента
+#     print("---")
+# print("Конец цикла")
 
-print(min_value_rows)
-print(min_index_rows)
-print(max_value_rows)
-print(max_index_rows)
+# list_ = [-5, 2, 4, 8, 12, -7, 5]
+# # Объявим переменную, в которой будем хранить индекс отрицательного элемента
+# index_negative = None
+#
+# for i, value in enumerate(list_):
+#     if list_[i] < 0:
+#         print("Отрицательное число: ", list_[i])
+#         index_negative = i  # перезаписываем значение индекса
+#         print("Новый индекс отрицательного числа: ", index_negative)
+#     else:
+#         print("Положительное число: ", list_[i])
+#     print("---")
+# print("Конец цикла")
+# print()
+# print("Ответ: индекс последнего отрицательного элемента = ", index_negative)
+
+# text = """
+# У лукоморья дуб зелёный;
+# Златая цепь на дубе том:
+# """
+#
+# text = text.lower()
+# text = text.replace(" ", "")
+# text = text.replace("\n", "")
+# print(text)
+# count = {}  # для подсчета символов и их количества
+# for char in text:
+#    if char in count:  # если символ уже встречался, то увеличиваем его количество на 1
+#        count[char] += 1
+#    else:
+#        count[char] = 1
+# for char, cnt in count.items():
+#    print(f"Символ {char} встречается {cnt} раз")
+
+# n = int(input("Введите число\n"))
+
+# while True:
+#     if n % 2 != 0:
+#         n = (n * 3 + 1) // 2
+#     else:
+#         n = n // 2
+#     print(n)
+#
+#     if n == 1:
+#         print("Done")
+#         break
+
+# heads = 35  # количество голов
+# legs = 94  # количество ног
+
+# for r in range(heads + 1):  # количество кроликов
+#     for ph in range(heads + 1):  # количество фазанов
+#         #  если суммарное количество голов превышено или ног превышено, то переходим на следующий шаг цикла
+#         if (r + ph) > heads or \
+#             (r * 4 + ph * 2) > legs:
+#             continue
+#         if (r + ph) == heads and (r * 4 + ph * 2) == legs:
+#             print("Количество кроликов", r)
+#             print("Количество фазанов", ph)
+#             print("---")
+# some_var = (2,)
+# if some_var is None:
+#     print("NoneType")
+# else:
+#     print(type(some_var))
+
+# squares = [(i, i**2) for i in range(1,11) if i % 2 == 1]
+# print(squares)
+#
+# M = [[i+j for j in range(5)] for i in range(5)]
+# print(M)
+#
+# T = [[i*j for j in range(1,11)] for i in range(1,11)]
+# print(T)
+
+# L = [int(input()) % 2 == 0 for i in range(5)]
+# print(any(L))
+
+N = int(input("ведите необходимое колличество билетов = "))
+# #
+result = 0  # и результирующую строку
+# #
+# #
+# if N > 0: # если количество посетителей еще больше нуля,
+for i in range(1, N+1):
+    age = int(input("ведите возраст посетителя номер " + str(i) + "- "))
+    if 18 <= age < 25:
+      result += 990
+    elif age >= 25:
+      result += 1390
+# N -= 1  # то уменьшаем  счетчик
+print("сумма к оплате - ", result)
+# else:
+#     N == 0   # иначе - записываем в результат
+
+
+
+# age = int(input())
+
+# if 18 <= age < 25:
+#     result += 990
+# elif age >= 25:
+#     result += 1390
+# print(result)
+
+# print("setvberbte = " + str(N))
